6bd14686df93749da306006e7b33a0fbf4ef0dd5
HelenePhilippot <h.e.philippot@gmail.com>
2019-06-17 11:18:42 +0200

    Ajout des repositories

---
+++ projetVideoClub/src/main/java/repository/ArticleRepository.java
@@ -0,0 +1,17 @@
+package repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import metier.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Integer>{
+public List<Article> findByNoArticle(Integer noArticle);
+public List<Article> findByNbDisques(Integer nbDisques);
+@Query("select s from Article s where s.article_type=:type")
+public List<Article> findByType(@Param("type")String type);
+}
