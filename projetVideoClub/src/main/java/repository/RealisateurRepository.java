584be06ddcefefcebf00ea811d3262c50bfa76ed
HelenePhilippot <h.e.philippot@gmail.com>
2019-06-17 11:51:28 +0200

    Modifs des repositories

--- projetVideoClub/src/main/java/repository/RealisateurRepository.java
+++ projetVideoClub/src/main/java/repository/RealisateurRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,9 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import metier.Grade;
-import metier.Soldat;
-
 public interface RealisateurRepository extends JpaRepository<Realisateur, Integer>{
+	public List<Realisateur> findByNom(String nom);
+	public List<Realisateur> findByPrenom(String prenom);
 
 }
