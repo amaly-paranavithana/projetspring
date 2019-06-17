82b365eabc51e20bea3f6c8e09a61c23561761b5
Martial <martial.pouzet@laposte.net>
2019-06-17 12:27:31 +0200

    classe film réalisateur + classe pour table de jointure

---
+++ projetVideoClub/src/main/java/metier_martial/FilmRealisateur.java
@@ -0,0 +1,49 @@
+package metier_martial;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="join_film_grade")
+public class FilmRealisateur {
+	
+	@EmbeddedId
+	private FilmRealisateurKey key;
+	
+	public FilmRealisateurKey getKey() {
+		return key;
+	}
+
+	public void setKey(FilmRealisateurKey key) {
+		this.key = key;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FilmRealisateur other = (FilmRealisateur) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ projetVideoClub/src/main/java/metier_martial/Realisateur.java
@@ -0,0 +1,73 @@
+package metier_martial;
+
+import javax.persistence.*;
+
+@Entity
+public class Realisateur {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String prenom;
+	private String nom;
+	
+	public Realisateur() {}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Realisateur other = (Realisateur) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (prenom == null) {
+			if (other.prenom != null)
+				return false;
+		} else if (!prenom.equals(other.prenom))
+			return false;
+		return true;
+	}
+	
+	
+
+}
