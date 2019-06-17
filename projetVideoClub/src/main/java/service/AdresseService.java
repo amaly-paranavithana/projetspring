a40b7377ede35aeec464ef73182a9ac81b7095ad
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-06-17 15:00:07 +0200

    création des enveloppes dans Adresseservice

--- projetVideoClub/src/main/java/service/AdresseService.java
+++ projetVideoClub/src/main/java/service/AdresseService.java
@@ -4,47 +4,58 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import metier.*;
 import repository.*;
 
+@Service
+public class AdresseService {
 
-	
-	@Service
-	public class AdresseService {
+	@Autowired
+	private AdresseRepository adresseRepository;
 
-		@Autowired
-		private AdresseRepository adresseRepository;
-		
-
-		public boolean insert(Adresse adresse) {
-			if (adresse.getId() == null || adresse.getId() != 0) {
-				return false;
-			}
-			adresseRepository.save(adresse);
-			return true;
+	public boolean insert(Adresse adresse) {
+		if (adresse.getId() == null || adresse.getId() != 0) {
+			return false;
 		}
+		adresseRepository.save(adresse);
+		return true;
+	}
 
-		public void delete(Adresse adresse) {
+	public void delete(Adresse adresse) {
 		Optional<Adresse> opt = adresseRepository.findById(adresse.getId());
 		if (opt.isPresent()) {
 			adresseRepository.delete(opt.get());
 		}
+	}
+
+	public void update(Adresse adresse) {
+
+		Optional<Adresse> opt = adresseRepository.findById(adresse.getId());
+		if (opt.isPresent()) {
+			adresseRepository.save(opt.get());
 		}
+	}
 
-	
-		public void update(Adresse adresse) {
+	public Adresse findById(Integer id) {
+		return adresseRepository.findById(id).get();
+	}
 
-			Optional<Adresse> opt = adresseRepository.findById(adresse.getId());
-			if (opt.isPresent()) {
-				adresseRepository.save(opt.get());
-				
-			}
-			
-		}
+	public List<Adresse> findByNumero(Integer numero) {
+		return adresseRepository.findByNumero(numero);
+	}
 
-		
-	
+	public List<Adresse> findByRue(String rue) {
+		return adresseRepository.findByRue(rue);
+	}
 
+	public List<Adresse> findByVille(String ville) {
+		return adresseRepository.findByVille(ville);
+	}
+
+	public List<Adresse> findByCp(String cp) {
+		return adresseRepository.findByCp(cp);
+	}
 }
